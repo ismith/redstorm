54f14c99e8f9f270418386aadc3b8279737e32b6
Ian Smith <ismith@metaforgotten.org>
2015-03-10 12:14:32 -0700

    Allow a topology jar to drop into a pry console.

--- lib/red_storm/topology_launcher.rb
+++ lib/red_storm/topology_launcher.rb
@@ -31,41 +31,52 @@ class TopologyLauncher
 
   java_signature 'void main(String[])'
   def self.main(args)
-    unless args.size > 1
+    unless args.size > 1 || args[0] == :pry
       puts("Usage: redstorm local|cluster topology_class_file_name")
+      puts("       OR redstorm pry")
       exit(1)
     end
 
     env = args[0].to_sym
-    class_path = args[1]
 
     launch_path = Dir.pwd
     $:.unshift File.expand_path(launch_path)
     $:.unshift File.expand_path(launch_path + '/lib')
     $:.unshift File.expand_path(launch_path + '/target/lib')
 
-    begin
-      require "#{class_path}"
-    rescue => ex
-      puts "Failed to load #{class_path}! (#{ex.inspect})"
-      puts ex.backtrace.join("\n")
-      # Re-raising to exit out of the scripting container
-      raise
-    end
+    if env == :pry
+      start_pry_console
+    else
+      class_path = args[1]
 
-    if RedStorm::Configuration.topology_class.nil? || !RedStorm::Configuration.topology_class.method_defined?(:start)
-      puts("\nERROR: invalid topology class. make sure your topology class is a subclass of one of the DSL topology classes or that your class sets RedStorm::Configuration.topology_class and defines the start method\n\n")
-      exit(1)
-    end
+      begin
+        require "#{class_path}"
+      rescue => ex
+        puts "Failed to load #{class_path}! (#{ex.inspect})"
+        puts ex.backtrace.join("\n")
+        # Re-raising to exit out of the scripting container
+        raise
+      end
 
-    topology_name = RedStorm::Configuration.topology_class.respond_to?(:topology_name) ? "/#{RedStorm::Configuration.topology_class.topology_name}" : ''
-    puts("RedStorm v#{RedStorm::VERSION} starting topology #{RedStorm::Configuration.topology_class.name}#{topology_name} in #{env.to_s} environment")
-    RedStorm::Configuration.topology_class.new.start(env)
+      if RedStorm::Configuration.topology_class.nil? || !RedStorm::Configuration.topology_class.method_defined?(:start)
+        puts("\nERROR: invalid topology class. make sure your topology class is a subclass of one of the DSL topology classes or that your class sets RedStorm::Configuration.topology_class and defines the start method\n\n")
+        exit(1)
+      end
+
+      topology_name = RedStorm::Configuration.topology_class.respond_to?(:topology_name) ? "/#{RedStorm::Configuration.topology_class.topology_name}" : ''
+      puts("RedStorm v#{RedStorm::VERSION} starting topology #{RedStorm::Configuration.topology_class.name}#{topology_name} in #{env.to_s} environment")
+      RedStorm::Configuration.topology_class.new.start(env)
+    end
   end
 
   private
 
   def self.camel_case(s)
     s.to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
+  end
+
+  def self.start_pry_console
+    require 'pry'
+    Pry.start
   end
 end
